5fe02bc37bfa22782a3f0e9ebba4fb57cf78ea19
Andrey Gagarin <andreygagarinchel@gmail.com>
2020-06-24 19:01:14 +0500

    Added animation for hexagon. Added two different materials for animation purpose

--- Cylinder022.gd
+++ Cylinder022.gd
@@ -1,5 +1,7 @@
 extends MeshInstance
-
+onready var myAnimation = get_node("AnimationPlayer")
+onready var selected = preload("res://materials/SelectedCylinder.tres")
+onready var usual = preload("res://materials/UsualCylinder.tres")
 
 func _on_StaticBody_input_event(camera, event, click_position, click_normal, shape_idx):
 	if event is InputEventMouseButton and event.pressed:
@@ -10,3 +12,12 @@ func _on_StaticBody_input_event(camera, event, click_position, click_normal, sha
 			var v = Vector3(0, -0.1, 0)
 			self.translate_object_local(v)
 
+
+
+func _on_StaticBody_mouse_entered():
+	self.set_surface_material(0,selected)
+	myAnimation.play("color")
+
+
+func _on_StaticBody_mouse_exited():
+	self.set_surface_material(0,usual)
